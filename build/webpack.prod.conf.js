94693d7da1ffee9cfb19e534f2743640b2ba0f6e
Hopetree <zlwork2014@163.com>
2019-08-31 00:25:34 +0800

    add favicon.png

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -63,6 +63,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
+      favicon: 'src/assets/favicon.png',
       inject: true,
       minify: {
         removeComments: true,
