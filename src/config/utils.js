c9ce899c78f6cc16ab552adb6f34ced5073b234a
Hopetree <zlwork2014@163.com>
2019-09-06 20:10:39 +0800

    add config

---
+++ src/config/utils.js
@@ -0,0 +1,44 @@
+const img_path = '/static/img/'
+
+const searchList = {
+    doge: {
+        title: 'Doge',
+        url: 'https://www.dogedoge.com/results',
+        key: 'q',
+        icon: img_path + 'icon_doge.ico'
+    },
+    baidu: {
+        title: '百度',
+        url: 'https://www.baidu.com/s',
+        key: 'wd',
+        icon: img_path + 'icon_baidu.png'
+    },
+    bing: {
+        title: '必应',
+        url: 'https://cn.bing.com/search',
+        key: 'q',
+        icon: img_path + 'icon_bing.png'
+    },
+    sogou: {
+        title: '搜狗',
+        url: 'https://www.sogou.com/web',
+        key: 'query',
+        icon: img_path + 'icon_sogou.png'
+    },
+    so: {
+        title: '360搜索',
+        url: 'https://www.so.com/s',
+        key: 'q',
+        icon: img_path + 'icon_360.png'
+    },
+    google: {
+        title: 'Google',
+        url: 'https://www.google.com/search',
+        key: 'q',
+        icon: img_path + 'icon_google.png'
+    }
+}
+
+export default {
+    searchList
+}
